c852c1e28100c7854c798e76761d659251f34e6b
Denis Sirotkin <sirotkin@denis-02.ru>
2024-08-12 09:56:11 +0300

    append 3 homework

--- 3_taxis/main.go
+++ 3_taxis/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"awesomeProject/3_taxis/mock"
 	"awesomeProject/3_taxis/taxis"
+	"context"
 	"fmt"
+	"golang.org/x/sync/errgroup"
 	"sort"
 	"sync"
+	"time"
 )
 
 type TaxiService interface {
@@ -15,22 +18,87 @@ type TaxiService interface {
 
 func calcRide(a, b taxis.Point, taxiServices []TaxiService) {
 	var wg sync.WaitGroup
-	var answers = []taxis.Ride{}
+	var mu sync.Mutex
+	var answers []taxis.Ride
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
 	for _, t := range taxiServices {
 		wg.Add(1)
-		go func() {
+		go func(service TaxiService) {
 			defer wg.Done()
-			taxiPrice, err := t.Calc(a, b)
-			if err != nil {
-				return
+
+			// Канал для получения результата или ошибки
+			resultChan := make(chan *taxis.Ride)
+			errorChan := make(chan error)
+
+			go func() {
+				taxiPrice, err := service.Calc(a, b)
+				if err != nil {
+					errorChan <- err
+					return
+				}
+				resultChan <- taxiPrice
+			}()
+
+			select {
+			case <-ctx.Done():
+				fmt.Println("The waiting time has expired")
+			case taxiPrice := <-resultChan:
+				mu.Lock()
+				answers = append(answers, *taxiPrice)
+				mu.Unlock()
+			case err := <-errorChan:
+				fmt.Println("Error from service:", err)
 			}
-			answers = append(answers, *taxiPrice)
-		}()
+			close(resultChan)
+			close(errorChan)
+		}(t)
 	}
 	wg.Wait()
+
+	// Сортировка результатов по цене
 	sort.SliceStable(answers, func(i, j int) bool {
 		return answers[i].Price < answers[j].Price
 	})
+
+	// Вывод результатов
+	for _, answer := range answers {
+		fmt.Println(answer)
+	}
+}
+
+func calcRideWithErrGroup(a, b taxis.Point, taxiServices []TaxiService) {
+	var answers []taxis.Ride
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	mu := &sync.Mutex{}
+
+	for _, t := range taxiServices {
+		service := t
+		go eg.Go(func() error {
+			taxiPrice, err := service.Calc(a, b)
+			if err != nil {
+				return err
+			}
+			mu.Lock()
+			answers = append(answers, *taxiPrice)
+			mu.Unlock()
+			return nil
+		})
+	}
+
+	if err := eg.Wait(); err != nil {
+		fmt.Println(err)
+	}
+
+	sort.SliceStable(answers, func(i, j int) bool {
+		return answers[i].Price < answers[j].Price
+	})
+
 	for _, answer := range answers {
 		fmt.Println(answer)
 	}
@@ -52,4 +120,5 @@ func main() {
 		mock.NewMockYandexTaxi(),
 	}
 	calcRide(p1, p2, taxiServices)
+	calcRideWithErrGroup(p1, p2, taxiServices)
 }
